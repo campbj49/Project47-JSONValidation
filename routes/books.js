1f7ed901bd6828bec94c78cebc66d66d61572365
John Campbell <campbj49@gmail.com>
2023-09-06 16:25:57 -0400

    book route tests working

--- routes/books.js
+++ routes/books.js
@@ -57,6 +57,18 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    //use validation example from API Validation note sheet
+    const result = jsonschema.validate(req.body, bookSchema);
+  
+    if (!result.valid) {
+      // pass validation errors to error handler
+      //  (the "stack" key is generally the most useful)
+      let listOfErrors = result.errors.map(error => error.stack);
+      let error = new ExpressError(listOfErrors, 400);
+      return next(error);
+    }
+  
+    // at this point in code, we know we have a valid payload
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
